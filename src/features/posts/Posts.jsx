6690fd306f88de3af0338762456a41683fba6c6d
Aditya Agrawal <aditya.agrawal0309@gmail.com>
2021-11-20 23:06:01 +0530

    feat: sort posts

--- src/features/posts/Posts.jsx
+++ src/features/posts/Posts.jsx
@@ -1,8 +1,15 @@
 import { useSelector } from "react-redux";
-import { selectAllPosts } from "../index";
+import { selectAllPosts, selectFetchedUser } from "../index";
 
 export function Posts() {
   const posts = useSelector(selectAllPosts);
+  const fetchedUser = useSelector(selectFetchedUser);
+
+  const sortedPosts = posts.filter(
+    (post) =>
+      fetchedUser?.following.includes(post?.user?._id) ||
+      post?.user?._id === fetchedUser?._id
+  );
 
   if (!posts.length) {
     return <div className="loader"></div>;
@@ -10,8 +17,8 @@ export function Posts() {
 
   return (
     <div className="flex flex-col items-center">
-      {posts &&
-        posts.map((post) => (
+      {sortedPosts &&
+        sortedPosts.map((post) => (
           <div className="post-card" key={post._id}>
             <p className="font-bold"> {post?.user?.username} </p>
             <p> {post?.content} </p>
